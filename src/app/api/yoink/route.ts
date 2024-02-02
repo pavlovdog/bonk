7da70736daf0392b816fdaa2962158e56fb954d0
pavlovdog <pavlovdog.moscow@gmail.com>
2024-02-02 20:28:24 +0400

    Fast gas

--- src/app/api/yoink/route.ts
+++ src/app/api/yoink/route.ts
@@ -101,8 +101,8 @@ const sendYoink = async (message: Message) => {
     nonce,
     initCode: '0x' as `0x${string}`,
     callData,
-    maxFeePerGas: gasPrice.standard.maxFeePerGas,
-    maxPriorityFeePerGas: gasPrice.standard.maxPriorityFeePerGas,
+    maxFeePerGas: gasPrice.fast.maxFeePerGas,
+    maxPriorityFeePerGas: gasPrice.fast.maxPriorityFeePerGas,
     // dummy signature, needs to be there so the SimpleAccount doesn't immediately revert because of invalid signature length
     signature:
       "0xa15569dd8f8324dbeabf8073fdec36d4b754f53ce5901e283c6de79af177dc94557fa3c9922cd7af2a96ca94402d35c39f266925ee6407aeb32b31d76978d4ba1c" as Hex,
